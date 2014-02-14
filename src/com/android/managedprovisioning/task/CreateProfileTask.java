aa1355da5f6725dcaa27eff8c7c3224e0195e277
Nicolas Prevot <nprevot@google.com>
2014-02-14 09:00:37 +0000

    Retain only requiredForAllUsers apps when creating a managed profile.
    When a managed profile is created, system apps are installed for the new user by default.
    This change removes all system apps except for those marked as requiredForAllUsers in their manifest.
    Additionally, a few hard-coded system apps are retained as a temporary measure to enable development & testing.
    These will be removed in a later CL.
    
    Change-Id: Ic24b2d5ca9d119c2ccfd7ef34ff7d298594c9434

--- src/com/android/managedprovisioning/task/CreateProfileTask.java
+++ src/com/android/managedprovisioning/task/CreateProfileTask.java
@@ -22,11 +22,20 @@ import android.content.pm.UserInfo;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserManager;
+import android.content.pm.ApplicationInfo;
+import java.util.List;
+import android.util.Log;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import com.android.managedprovisioning.ManagedProvisioningActivity.ProvisioningState;
 import com.android.managedprovisioning.Preferences;
 import com.android.managedprovisioning.ProvisionLogger;
+import com.android.managedprovisioning.R;
+import com.android.managedprovisioning.ConfigureUserActivity;
+import java.util.Arrays;
 
+import java.util.HashSet;
 /**
  * This tasks
  *     - creates a secondary user
@@ -36,8 +45,13 @@ import com.android.managedprovisioning.ProvisionLogger;
  */
 public class CreateProfileTask extends ProvisionTask {
 
-    public CreateProfileTask() {
+    private Context mContext;
+    private IPackageManager mIpm;
+    private UserInfo mManagedProfileUserInfo;
+
+    public CreateProfileTask(Context context) {
         super("Create Profile");
+        mContext = context;
     }
 
     @Override
@@ -46,7 +60,7 @@ public class CreateProfileTask extends ProvisionTask {
         // Preferences that persist initial parameters of provisioning.
         Preferences prefs = mTaskManager.getPreferences();
 
-        IPackageManager ipm = IPackageManager.Stub.asInterface(ServiceManager.getService("package"));
+        mIpm = IPackageManager.Stub.asInterface(ServiceManager.getService("package"));
         UserManager userManager = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
         String mdmPackageName = prefs.getStringProperty(Preferences.MDM_PACKAGE_KEY);
         String defaultManagedProfileName = prefs.getStringProperty(
@@ -55,22 +69,25 @@ public class CreateProfileTask extends ProvisionTask {
 
         // Create the new user for the managed profile
         // TODO make this a related user.
-        UserInfo user = userManager.createUser(defaultManagedProfileName, 0);
+        mManagedProfileUserInfo = userManager.createUser(defaultManagedProfileName, 0);
+
+        deleteNonRequiredAppsForManagedProfile();
 
         // Install the mdm for the new profile.
         // TODO: Include proper error handling for both catch blocks to report back to the mdm.
         try {
-            ipm.installExistingPackageAsUser(mdmPackageName, user.id);
+            mIpm.installExistingPackageAsUser(mdmPackageName, mManagedProfileUserInfo.id);
         } catch (RemoteException e) {
             ProvisionLogger.logd("RemoteException, installing the mobile device management application "
                     + "for the managed profile failed.");
         }
 
+
         // TODO: Set the mdm as the profile owner of the managed profile.
 
         // Remove the mdm from the primary user.
         try {
-            ipm.deletePackageAsUser(mdmPackageName, null, userManager.getUserHandle(), 0);
+            mIpm.deletePackageAsUser(mdmPackageName, null, userManager.getUserHandle(), 0);
         } catch (Exception e) {
             ProvisionLogger.logd("RemoteException, removing the mobile device management application "
                     + "from the primary user failed failed.");
@@ -79,6 +96,48 @@ public class CreateProfileTask extends ProvisionTask {
 
         mTaskManager.registerProvisioningState(ProvisioningState.CREATE_PROFILE, "");
         onSuccess();
+    }
+
+    public void deleteNonRequiredAppsForManagedProfile() {
+        List<ApplicationInfo> allApps = null;
+        try {
+            allApps = mIpm.getInstalledApplications(
+                    PackageManager.GET_UNINSTALLED_PACKAGES, mManagedProfileUserInfo.id).getList();
+        } catch (RemoteException e) {
+            ProvisionLogger.logd("RemoteException when getting the installed applications for the "
+                    + "managed profile");
+        }
+
+        //TODO: Remove hardcoded list of required apps. This is just a temporary list to aid
+        //development and testing.
+
+        HashSet<String> requiredApps = new HashSet<String> (Arrays.asList(
+                mContext.getResources().getStringArray(R.array.required_managedprofile_apps)));
+
+        for (ApplicationInfo app : allApps) {
+            PackageInfo packageInfo = null;
+            try {
+                packageInfo = mIpm.getPackageInfo(app.packageName,
+                        PackageManager.GET_UNINSTALLED_PACKAGES | PackageManager.GET_SIGNATURES,
+                        mManagedProfileUserInfo.id);
+            } catch (RemoteException e) {
+                ProvisionLogger.logd("RemoteException when getting package info for "
+                        + app.packageName + " for the managed profile");
+            }
+
+            boolean isRequired = packageInfo.requiredForAllUsers
+                    || requiredApps.contains(app.packageName);
+
+            if (!isRequired) {
+                try {
+                    mIpm.deletePackageAsUser(app.packageName, null, mManagedProfileUserInfo.id,
+                            PackageManager.DELETE_SYSTEM_APP);
+                } catch (RemoteException e) {
+                    ProvisionLogger.logd("RemoteException when deleting " + app.packageName
+                            + " for the managed profile");
+                }
+            }
+        }
     }
 
     @Override
