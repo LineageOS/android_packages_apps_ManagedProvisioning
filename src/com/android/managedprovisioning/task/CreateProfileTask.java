7932c4ebcc4bc353df955d9a8a437c4473ca4d63
Jessica Hummel <jhummel@google.com>
2014-02-18 12:29:05 +0000

    Remove unused import to fix the build.
    Change-Id: I264eab2f2661acf62af3b37e2e2d6d1f35bb0971

--- src/com/android/managedprovisioning/task/CreateProfileTask.java
+++ src/com/android/managedprovisioning/task/CreateProfileTask.java
@@ -17,25 +17,23 @@
 package com.android.managedprovisioning.task;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageManager;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.UserInfo;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserManager;
-import android.content.pm.ApplicationInfo;
-import java.util.List;
-import android.util.Log;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 
 import com.android.managedprovisioning.ManagedProvisioningActivity.ProvisioningState;
 import com.android.managedprovisioning.Preferences;
 import com.android.managedprovisioning.ProvisionLogger;
 import com.android.managedprovisioning.R;
-import com.android.managedprovisioning.ConfigureUserActivity;
-import java.util.Arrays;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 /**
  * This tasks
  *     - creates a secondary user
