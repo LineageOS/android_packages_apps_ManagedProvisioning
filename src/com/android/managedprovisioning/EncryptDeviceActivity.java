e3146e92028e1aee45082d1b925ff45feee789c8
Jessica Hummel <jhummel@google.com>
2014-05-21 15:09:21 +0100

    Adapt provisioning UI to mocks.
    Change-Id: I329e6768fea2ac49a3bb0fe166d8f2a0b4a88252

--- src/com/android/managedprovisioning/EncryptDeviceActivity.java
+++ src/com/android/managedprovisioning/EncryptDeviceActivity.java
@@ -23,15 +23,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import android.os.BatteryManager;
+import android.os.Bundle;
+import android.os.ServiceManager;
 import android.os.storage.IMountService;
 import android.os.storage.StorageManager;
 import android.os.RemoteException;
-import android.os.ServiceManager;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Activity to ask for permission to activate full-filesystem encryption.
