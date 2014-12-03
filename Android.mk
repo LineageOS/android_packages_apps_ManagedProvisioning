edd7f3d057d01e4402411f20e831268825f31f4e
Rubin Xu <rubinxu@google.com>
2014-12-03 17:52:08 +0000

    Add SUW style transition animation to provisioning flows
    Add sliding animation when starting activities, and cross-fading
    when going into interstitial screens.
    
    Bug: 18518260
    Change-Id: I3e7a8bf16cf3b0aaca58dce0576046feb4734b58

--- Android.mk
+++ Android.mk
@@ -7,7 +7,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    vendor/google/apps/SetupWizard/res-shared
 
 LOCAL_PACKAGE_NAME := ManagedProvisioning
 LOCAL_CERTIFICATE := platform
