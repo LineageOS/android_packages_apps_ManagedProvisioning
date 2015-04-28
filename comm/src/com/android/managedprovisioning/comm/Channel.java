698ebc9b40649182fec5725567fbb3ccd5ec12d2
Joe Delfino <jdelfino@google.com>
2015-04-28 13:27:17 -0400

    Allow Channel to be overridden
    Introduce a ChannelFactory and make Channel an interface. This allows
    clients to inject their own implementations of Channel. In
    particular, this allows clients to inject their own message parsing.
    
    Bug: 20724151
    
    Change-Id: I9648724f74426235ed77820ed1cddf7250987627

--- comm/src/com/android/managedprovisioning/comm/Channel.java
+++ comm/src/com/android/managedprovisioning/comm/Channel.java
@@ -16,116 +16,49 @@
 
 package com.android.managedprovisioning.comm;
 
-import com.android.managedprovisioning.comm.Bluetooth;
-import com.android.managedprovisioning.comm.Bluetooth.CommPacket;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import com.google.protobuf.nano.CodedInputByteBufferNano;
-import com.google.protobuf.nano.CodedOutputByteBufferNano;
 
 /**
- * A Channel allows communication over a SocketWrapper using Protobuf messages.
+ * A channel controls the I/O of a single socket. It handles reading from/writing to the socket, as
+ * well as parsing incoming messages and serializing outgoing messages.
  */
-public class Channel implements AutoCloseable {
-    protected final SocketWrapper mSocket;
+public interface Channel extends AutoCloseable {
 
-    // Used to synchronize writes while allowing simultaneous read/writing.
-    protected final Object mWriteLock = new Object();
+    /**
+     * Write a message to the socket.
+     * @param packet The message to be written
+     * @throws IOException If the write fails
+     */
+    void write(Bluetooth.CommPacket packet) throws IOException;
 
-    public Channel(SocketWrapper socket) {
-        mSocket = socket;
-    }
-
-    // GuardedBy(mWriteLock)
-    public void write(CommPacket packet)
-            throws IOException {
-        synchronized (mWriteLock) {
-            OutputStream outputStream = mSocket.getOutputStream();
-
-            int size = packet.getSerializedSize();
-            ProvisionCommLogger.logd("Sending message size: " + size);
-            int delimitSize = CodedOutputByteBufferNano.computeRawVarint32Size(size);
-            byte[] array = new byte[size + delimitSize];
-            CodedOutputByteBufferNano outputBuffer = CodedOutputByteBufferNano.newInstance(array);
-            outputBuffer.writeRawVarint32(size);
-            packet.writeTo(outputBuffer);
-            if (outputBuffer.spaceLeft() != 0) {
-                throw new IOException("Incorrect size calculated");
-            }
-            outputStream.write(array);
-
-            outputStream.flush();
-        }
-    }
-
-    public synchronized CommPacket read() throws IOException {
-        return read(mSocket.getInputStream());
-    }
-
-    @SuppressWarnings("unchecked")
-    protected synchronized CommPacket read(InputStream inputStream) throws IOException {
-        CodedInputByteBufferNano inputBuffer = readByteBuffer(inputStream);
-        try {
-            return readPacket(inputBuffer);
-        } catch (ClassCastException e) {
-            ProvisionCommLogger.loge("Incorrect type called for return value", e);
-            return null;
-        }
-    }
-
-    protected CodedInputByteBufferNano readByteBuffer(InputStream inputStream) throws IOException {
-        byte[] readBuffer = new byte[512];
-        int index = 0;
-        // Read bytes while the most significant bit is set.  The CodedInputByteBufferNano from
-        // proto-nano only reads up to 10 bytes, so we will do the same.
-        do {
-            while (inputStream.read(readBuffer, index, 1) <= 0);
-        } while ((readBuffer[index++] < 0) && (index < 10));
-
-        CodedInputByteBufferNano inputBuffer =
-                CodedInputByteBufferNano.newInstance(readBuffer, 0, index);
-        int size = inputBuffer.readRawVarint32();
-        byte[] buffer = new byte[size];
-        int readIndex = 0;
-        while (readIndex < size) {
-            int amount = inputStream.read(buffer, readIndex, size - readIndex);
-            if (amount > 0) {
-                readIndex += amount;
-            }
-        }
-        return CodedInputByteBufferNano.newInstance(buffer);
-    }
-
-    protected CommPacket readPacket(CodedInputByteBufferNano inputBuffer)
-            throws IOException {
-        CommPacket packet = Bluetooth.CommPacket.parseFrom(inputBuffer);
-        return packet;
-    }
+    /**
+     * Read a message from the socket.
+     * @return The parsed message
+     * @throws IOException If reading or parsing fails
+     */
+    Bluetooth.CommPacket read() throws IOException;
 
     @Override
-    public void close() {
-        try {
-            mSocket.close();
-        } catch (IOException ioe) {
-            ProvisionCommLogger.logi(ioe);
-        }
-    }
+    /**
+     * Close this socket
+     */
+    void close();
 
     /**
      * Determine if the socket connection held by this instance is connected.
      * @return {@code true} if this socket is connected.
      */
-    public boolean isConnected() {
-        return mSocket.isConnected();
-    }
+    boolean isConnected();
 
     /**
-     * Flushes the contents of the buffer.  For unbuffered channels, this does nothing.
+     * Flush the contents of the buffer.
      * @throws IOException
      */
-    public void flush() throws IOException {
-    }
+    void flush() throws IOException;
+
+    /**
+     * Called when an error which could be retried is encountered.
+     * @throws IOException
+     */
+    void reset() throws IOException;
 }
