6b735a28af288dee3c5812b51cb163a1bf9502fb
Tony Mak <tonymak@google.com>
2017-03-30 15:36:47 +0100

    Use goAsync instead of startService in receiver
    Test: runtest managed-provisioning
    Test: adb shell am make-uid-idle  --user 0 com.android.managedprovisioning
          && adb shell am broadcast -a android.intent.action.PRE_BOOT_COMPLETED -n com.android.managedprovisioning/com.android.managedprovisioning.ota.PreBootListener
          Observe there is no crash
    
    1. OtaController now runs in worker thread instead of main thread.
    2. PRE_BOOT_COMPLETE is bg broadcast, so framework give us 60s to run.
    
    Change-Id: Ie7cc622335618250ecffec03864e8b11a42669f0
    Fix: 36705375

--- src/com/android/managedprovisioning/ota/TaskExecutor.java
+++ src/com/android/managedprovisioning/ota/TaskExecutor.java
@@ -16,67 +16,25 @@
 
 package com.android.managedprovisioning.ota;
 
-import static com.android.internal.util.Preconditions.checkNotNull;
-
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-
-import com.android.internal.annotations.GuardedBy;
-import com.android.internal.annotations.VisibleForTesting;
-import com.android.managedprovisioning.common.Globals;
 import com.android.managedprovisioning.common.ProvisionLogger;
-import com.android.managedprovisioning.provisioning.ProvisioningService;
 import com.android.managedprovisioning.task.AbstractProvisioningTask;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Class that executes the provisioning tasks during the OTA process.
  */
 public class TaskExecutor implements AbstractProvisioningTask.Callback {
 
-    @VisibleForTesting
-    static final Intent SERVICE_INTENT = new Intent().setComponent(new ComponentName(
-            Globals.MANAGED_PROVISIONING_PACKAGE_NAME,
-            ProvisioningService.class.getName()));
-
-    @GuardedBy("this")
-    private List<AbstractProvisioningTask> mOngoingTasks = new ArrayList<>();
-
-    private final Context mContext;
-
-    public TaskExecutor(Context context) {
-        mContext = checkNotNull(context);
-    }
-
     public synchronized void execute(int userId, AbstractProvisioningTask task) {
-        if (mOngoingTasks.isEmpty()) {
-            mContext.startService(SERVICE_INTENT);
-        }
-
-        mOngoingTasks.add(task);
         task.run(userId);
     }
 
     @Override
     public void onSuccess(AbstractProvisioningTask task) {
         ProvisionLogger.logd("Task ran successfully: " + task.getClass().getSimpleName());
-        taskCompleted(task);
     }
 
     @Override
     public void onError(AbstractProvisioningTask task, int errorMsg) {
         ProvisionLogger.logd("Error running task: " + task.getClass().getSimpleName());
-        taskCompleted(task);
-    }
-
-    private synchronized void taskCompleted(AbstractProvisioningTask task) {
-        mOngoingTasks.remove(task);
-
-        if (mOngoingTasks.isEmpty()) {
-            mContext.stopService(SERVICE_INTENT);
-        }
     }
 }
