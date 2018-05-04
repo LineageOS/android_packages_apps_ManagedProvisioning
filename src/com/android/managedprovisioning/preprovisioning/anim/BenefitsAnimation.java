69b0f18d4eca2dca6b1d898039d937005ee5bdbe
arangelov <arangelov@google.com>
2018-05-04 14:29:05 +0100

    Calculate the widths of the label translation animation dynamically instead of hardcoding them.
    This takes care of the problem where if you maximize the display size
    from Settings, the text goes out of bounds without wrapping to the next
    line.
    
    Screenshot: https://screenshot.googleplex.com/7B6JYQmLRdr.png
    
    Bug: 78445086
    Test: Manual
    Change-Id: Ied023ae6a0f9b5c0dc2acd8a956a1f5b0aa85f32

--- src/com/android/managedprovisioning/preprovisioning/anim/BenefitsAnimation.java
+++ src/com/android/managedprovisioning/preprovisioning/anim/BenefitsAnimation.java
@@ -21,6 +21,8 @@ import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.annotation.AnimRes;
+import android.annotation.InterpolatorRes;
 import android.app.Activity;
 import android.graphics.drawable.Animatable2;
 import android.graphics.drawable.AnimatedVectorDrawable;
@@ -29,17 +31,24 @@ import androidx.annotation.NonNull;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.managedprovisioning.R;
 import com.android.managedprovisioning.model.CustomizationParams;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * <p>Drives the animation showing benefits of having a Managed Profile.
  * <p>Tightly coupled with the {@link R.layout#intro_animation} layout.
+ * The animation consists of 4 horizontally scrolling text labels, placed horizontally.
+ * Each label's horizontal translation is * <code>i*screenWidth/2</code>, where i is the
+ * number of the label (0, 1, 2 or 3) and screenWidth is the actual screen width. We need to
+ * calculate the views' widths dynamically because we want them to fit the display width.
  */
 public class BenefitsAnimation {
     /** Array of Id pairs: {{@link ObjectAnimator}, {@link TextView}} */
@@ -47,8 +56,7 @@ public class BenefitsAnimation {
             {R.anim.text_scene_0_animation, R.id.text_0},
             {R.anim.text_scene_1_animation, R.id.text_1},
             {R.anim.text_scene_2_animation, R.id.text_2},
-            {R.anim.text_scene_3_animation, R.id.text_3},
-            {R.anim.text_scene_master_animation, R.id.text_master}};
+            {R.anim.text_scene_3_animation, R.id.text_3}};
 
     private static final int[] SLIDE_CAPTION_TEXT_VIEWS = {
             R.id.text_0, R.id.text_1, R.id.text_2, R.id.text_3};
@@ -58,9 +66,11 @@ public class BenefitsAnimation {
 
     private static final int SLIDE_COUNT = 3;
     private static final int ANIMATION_ORIGINAL_WIDTH_PX = 1080;
+    private static final int TRANSLATION_DURATION_MS = 1001;
+    private static final float MASTER_CONTAINER_DISPLAY_WIDTH_MULTIPLIER = 2.5f;
 
     private final AnimatedVectorDrawable mTopAnimation;
-    private final Animator mTextAnimation;
+    private Animator mTextAnimation;
     private final Activity mActivity;
 
     private boolean mStopped;
@@ -76,7 +86,6 @@ public class BenefitsAnimation {
                     "Wrong number of slide captions. Expected: " + SLIDE_COUNT);
         }
         mActivity = checkNotNull(activity);
-        mTextAnimation = checkNotNull(assembleTextAnimation());
         applySlideCaptions(captions);
         applyContentDescription(contentDescription);
 
@@ -84,11 +93,15 @@ public class BenefitsAnimation {
 
         mTopAnimation = checkNotNull(extractAnimationFromImageView(ID_ANIMATED_GRAPHIC));
 
-        // chain all animations together
-        chainAnimations();
-
         // once the screen is ready, adjust size
         mActivity.findViewById(android.R.id.content).post(this::adjustToScreenSize);
+        mActivity.findViewById(android.R.id.content).post(this::prepareAnimations);
+    }
+
+    private void prepareAnimations() {
+        mTextAnimation = checkNotNull(assembleTextAnimation());
+        // chain all animations together
+        chainAnimations();
     }
 
     private void setTopInfoDrawable(CustomizationParams customizationParams) {
@@ -124,6 +137,8 @@ public class BenefitsAnimation {
             return;
         }
 
+        setupLabelDimensions();
+
         ImageView animatedInfo = mActivity.findViewById(R.id.animated_info);
         int widthPx = animatedInfo.getWidth();
         float scaleRatio = (float) widthPx / ANIMATION_ORIGINAL_WIDTH_PX;
@@ -134,15 +149,6 @@ public class BenefitsAnimation {
         int adjustedHeight = (int) (originalHeight * scaleRatio);
         layoutParams.height = adjustedHeight;
         animatedInfo.setLayoutParams(layoutParams);
-
-        // adjust captions size only if downscaling
-        if (scaleRatio < 1) {
-            for (int textViewId : SLIDE_CAPTION_TEXT_VIEWS) {
-                View view = mActivity.findViewById(textViewId);
-                view.setScaleX(scaleRatio);
-                view.setScaleY(scaleRatio);
-            }
-        }
 
         // if the content is bigger than the screen, try to shrink just the animation
         int offset = adjustedHeight - originalHeight;
@@ -158,6 +164,32 @@ public class BenefitsAnimation {
                 layoutParams.height = targetHeight;
                 animatedInfo.setLayoutParams(layoutParams);
             }
+        }
+    }
+
+    private void setupLabelDimensions() {
+        final ImageView animatedInfo = mActivity.findViewById(R.id.animated_info);
+        final int width = animatedInfo.getWidth();
+        final FrameLayout textAnimationViewport =
+                mActivity.findViewById(R.id.text_animation_viewport);
+        final LayoutParams viewportParams = textAnimationViewport.getLayoutParams();
+        viewportParams.width = width;
+        textAnimationViewport.setLayoutParams(viewportParams);
+
+        final FrameLayout textMasterContainer = mActivity.findViewById(R.id.text_master);
+        final LayoutParams masterContainerParams = textMasterContainer.getLayoutParams();
+        masterContainerParams.width = (int) (width * MASTER_CONTAINER_DISPLAY_WIDTH_MULTIPLIER);
+        textMasterContainer.setLayoutParams(masterContainerParams);
+
+        int translation = 0;
+        final int step = width / 2;
+        for (int textViewId : SLIDE_CAPTION_TEXT_VIEWS) {
+            final View textView = mActivity.findViewById(textViewId);
+            final LayoutParams layoutParams = textView.getLayoutParams();
+            layoutParams.width = width;
+            textView.setLayoutParams(layoutParams);
+            textView.setTranslationX(translation);
+            translation += step;
         }
     }
 
@@ -199,16 +231,50 @@ public class BenefitsAnimation {
      * @return {@link AnimatorSet} responsible for the animated text
      */
     private AnimatorSet assembleTextAnimation() {
-        Animator[] animators = new Animator[ID_ANIMATION_TARGET.length];
+        Animator[] animators = new Animator[ID_ANIMATION_TARGET.length + 1];
         for (int i = 0; i < ID_ANIMATION_TARGET.length; i++) {
             int[] instance = ID_ANIMATION_TARGET[i];
             animators[i] = AnimatorInflater.loadAnimator(mActivity, instance[0]);
             animators[i].setTarget(mActivity.findViewById(instance[1]));
         }
+        animators[ID_ANIMATION_TARGET.length] = getTranslationAnimatorSet();
 
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(animators);
         return animatorSet;
+    }
+
+    /**
+     * Creates and returns a sequential horizontal translation animation of the benefits labels.
+     */
+    private AnimatorSet getTranslationAnimatorSet() {
+        ImageView animatedInfo = mActivity.findViewById(R.id.animated_info);
+        final int width = animatedInfo.getWidth();
+        AnimatorSet translationSet = new AnimatorSet();
+        View textContainer = mActivity.findViewById(R.id.text_master);
+        List<Animator> animators = new ArrayList<>();
+        animators.add(getTranslationAnimator(textContainer, 0, 0,
+                android.R.interpolator.linear));
+        animators.add(getTranslationAnimator(textContainer, 0, -width/2,
+                android.R.interpolator.fast_out_slow_in));
+        animators.add(getTranslationAnimator(textContainer, -width/2, -width/2,
+                android.R.interpolator.linear));
+        animators.add(getTranslationAnimator(textContainer, -width/2, -width,
+                android.R.interpolator.fast_out_slow_in));
+        animators.add(getTranslationAnimator(textContainer, -width, -width,
+                android.R.interpolator.linear));
+        animators.add(getTranslationAnimator(textContainer, -width, (int) (-width * 1.5),
+                android.R.interpolator.fast_out_slow_in));
+        translationSet.playSequentially(animators);
+        return translationSet;
+    }
+
+    private Animator getTranslationAnimator(View container, float from, float to,
+            @AnimRes @InterpolatorRes int interpolator) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(container, "translationX", from, to);
+        animator.setDuration(TRANSLATION_DURATION_MS);
+        animator.setInterpolator(AnimationUtils.loadInterpolator(mActivity, interpolator));
+        return animator;
     }
 
     /**
