a706d9d834665ddc0553d3ae622434ca94ad0eb7
Alan Stokes <alanstokes@google.com>
2019-11-22 12:30:28 +0000

    Remove unused import.
    I'm about to delete this constant, so the import needs to go.
    
    Bug: 135751498
    Test: Still builds.
    Change-Id: Ia56e91f20e5b836404ca842a2c135e8ccd0c5145

--- src/com/android/managedprovisioning/common/SettingsFacade.java
+++ src/com/android/managedprovisioning/common/SettingsFacade.java
@@ -17,7 +17,6 @@
 package com.android.managedprovisioning.common;
 
 import static android.provider.Settings.Global.DEVICE_PROVISIONED;
-import static android.provider.Settings.Global.PACKAGE_VERIFIER_ENABLE;
 import static android.provider.Settings.Secure.MANAGED_PROFILE_CONTACT_REMOTE_SEARCH;
 import static android.provider.Settings.Secure.CROSS_PROFILE_CALENDAR_ENABLED;
 import static android.provider.Settings.Secure.USER_SETUP_COMPLETE;
