3187fbc5eafde19b807c4c71236e49875e87c077
kholoud mohamed <kholoudm@google.com>
2020-02-28 17:58:38 +0000

    Move logic for notification to boot time.
    This deals with edge cases around when managed profile is created which
    would have meant the consent was not shown. It also means that if an app
    is updated via play and becomes configurable the notification will be
    shown on next boot.
    
    Fixes: 150113581
    Test: atest UpdateInteractAcrossProfilesAppOpTaskTest
    Test: atest CrossProfileConsentActivityRoboTest
    Test: atest CrossProfileAppsNotificationControllerTest
    Test: atest ManagedProvisioningSharedPreferencesTest
    Change-Id: I51d1303372d2f6e8af47081b4a4f5d829673e00b

--- tests/robotests/src/com/android/managedprovisioning/ota/CrossProfileAppsNotificationControllerTest.java
+++ tests/robotests/src/com/android/managedprovisioning/ota/CrossProfileAppsNotificationControllerTest.java
@@ -19,12 +19,14 @@ package com.android.managedprovisioning.ota;
 import static com.google.common.truth.Truth.assertThat;
 import static android.content.pm.UserInfo.FLAG_MANAGED_PROFILE;
 
-import static org.junit.Assert.fail;
 import static org.robolectric.Shadows.shadowOf;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
+import android.content.pm.CrossProfileApps;
+import android.os.UserHandle;
 import android.os.UserManager;
 
 import androidx.test.core.app.ApplicationProvider;
@@ -34,27 +36,38 @@ import com.android.managedprovisioning.common.ManagedProvisioningSharedPreferenc
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowProcess;
+
+import java.util.Collections;
+import java.util.Set;
 
 @RunWith(RobolectricTestRunner.class)
 public class CrossProfileAppsNotificationControllerTest {
 
     private static final int MY_USER_ID = 0;
     private static final int OTHER_USER_ID = 5;
+    private static final int OTHER_USER_UID = UserHandle.PER_USER_RANGE * OTHER_USER_ID;
+
+    private static final Set<String> CONFIGURABLE_PACKAGES = Set.of("CONFIGURABLE_PACKAGE");
+    private static final Set<String> NON_CONFIGURABLE_PACKAGES = Set.of("NON_CONFIGURABLE_PACKAGE");
 
     private final Context mContext = ApplicationProvider.getApplicationContext();
     private final UserManager mUserManager = mContext.getSystemService(UserManager.class);
+    private final CrossProfileApps mCrossProfileApps =
+            mContext.getSystemService(CrossProfileApps.class);
+    private final DevicePolicyManager mDevicePolicyManager =
+            mContext.getSystemService(DevicePolicyManager.class);
     private final NotificationManager mNotificationManager =
             mContext.getSystemService(NotificationManager.class);
     private final ManagedProvisioningSharedPreferences mManagedProvisioningSharedPreferences =
             new ManagedProvisioningSharedPreferences(mContext);
-
     private final CrossProfileAppsNotificationController mNotificationController =
             new CrossProfileAppsNotificationController(mContext);
 
     @Test
     public void onPrimaryProfile_noManagedProfile_doesNotShow() {
         // We default to no managed profile
-        setConsentDone();
+        setWhitelistedPackages(Collections.emptySet());
 
         mNotificationController.maybeShowPermissionsNotification();
 
@@ -62,51 +75,81 @@ public class CrossProfileAppsNotificationControllerTest {
     }
 
     @Test
-    public void onPrimaryProfileWithManagedProfile_consentDone_doesNotShow() {
+    public void onPrimaryProfileWithManagedProfile_noPackagesToConsent_doesNotShow() {
         setRunningOnParentProfile();
-        setConsentNotDone();
+        setWhitelistedPackages(Collections.emptySet());
 
         mNotificationController.maybeShowPermissionsNotification();
 
         assertThat(shadowOf(mNotificationManager).getAllNotifications()).isEmpty();
+    }
+
+    @Test
+    public void onPrimaryProfileWithManagedProfile_allPackagesConsented_doesNotShow() {
+        setRunningOnParentProfile();
+        setWhitelistedPackages(CONFIGURABLE_PACKAGES);
+        setConfigurablePackages(CONFIGURABLE_PACKAGES);
+        setConsentedPackages(CONFIGURABLE_PACKAGES);
+
+        mNotificationController.maybeShowPermissionsNotification();
+
+        assertThat(shadowOf(mNotificationManager).getAllNotifications()).isEmpty();
+    }
+
+    @Test
+    public void onPrimaryProfileWithManagedProfile_nonConfigurableWhitelistedPackages_doesNotShow() {
+        setRunningOnParentProfile();
+        setWhitelistedPackages(NON_CONFIGURABLE_PACKAGES);
+
+        mNotificationController.maybeShowPermissionsNotification();
+
+        assertThat(shadowOf(mNotificationManager).getAllNotifications()).isEmpty();
+    }
+
+    @Test
+    public void onPrimaryProfileWithManagedProfile_nonConsentedPackages_shows() {
+        setRunningOnParentProfile();
+        setWhitelistedPackages(CONFIGURABLE_PACKAGES);
+        setConfigurablePackages(CONFIGURABLE_PACKAGES);
+
+        mNotificationController.maybeShowPermissionsNotification();
+
+        Notification notification = shadowOf(mNotificationManager).getNotification(/* id= */ 0);
+        assertThat(shadowOf(notification).getContentTitle())
+                .isEqualTo("Connect work & personal apps");
     }
 
     @Test
     public void onManagedProfile_doesNotShow() {
         setRunningOnManagedProfile();
-        setConsentNotDone();
+        setWhitelistedPackages(CONFIGURABLE_PACKAGES);
+        setConfigurablePackages(CONFIGURABLE_PACKAGES);
 
         mNotificationController.maybeShowPermissionsNotification();
 
         assertThat(shadowOf(mNotificationManager).getAllNotifications()).isEmpty();
     }
 
-    @Test
-    public void onPrimaryProfileWithManagedProfile_consentNotDone_shows() {
-         setRunningOnParentProfile();
-         setConsentDone();
-
-        mNotificationController.maybeShowPermissionsNotification();
-
-        Notification notification = shadowOf(mNotificationManager).getNotification(/* id= */ 0);
-        assertThat(shadowOf(notification).getContentTitle()).isEqualTo("Connect work & personal apps");
-
-    }
-
     private void setRunningOnParentProfile() {
-        shadowOf(mUserManager)
-                .addProfile(MY_USER_ID, OTHER_USER_ID, "otherUser", FLAG_MANAGED_PROFILE);
+        shadowOf(mUserManager).addProfile(
+                MY_USER_ID, OTHER_USER_ID, /*profileName= */"otherUser", FLAG_MANAGED_PROFILE);
     }
 
     private void setRunningOnManagedProfile() {
-        shadowOf(mUserManager).addProfile(MY_USER_ID, MY_USER_ID, "myUser", FLAG_MANAGED_PROFILE);
+        shadowOf(mUserManager).addProfile(
+                MY_USER_ID, OTHER_USER_ID, /*profileName= */"otherUser", FLAG_MANAGED_PROFILE);
+        ShadowProcess.setUid(OTHER_USER_UID);
     }
 
-    private void setConsentNotDone() {
-        mManagedProvisioningSharedPreferences.writeCrossProfileConsentDone(true);
+    private void setConsentedPackages(Set<String> packages) {
+        mManagedProvisioningSharedPreferences.writeConsentedCrossProfilePackages(packages);
     }
 
-    private void setConsentDone() {
-        mManagedProvisioningSharedPreferences.writeCrossProfileConsentDone(false);
+    private void setWhitelistedPackages(Set<String> packages) {
+        shadowOf(mDevicePolicyManager).setDefaultCrossProfilePackages(packages);
+    }
+
+    private void setConfigurablePackages(Set<String> packages) {
+        packages.forEach(p -> shadowOf(mCrossProfileApps).addCrossProfilePackage(p));
     }
 }
