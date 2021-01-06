2993781d5011ee12a40a4d5f57fbaac2ca1d9465
Antoan Angelov <arangelov@google.com>
2021-01-06 14:45:59 +0000

    Revert "Revert "Temporarily show a generic title in the ownershi..."
    Revert submission 13236640-revertMP
    
    Reason for revert: Now that the other components are fixed, this change should be safe to merge again
    Reverted Changes:
    I754fef216:Revert "Make sync auth use the admin-integrated fl...
    Ic42cabc95:Revert "Revert "Revert "Add tests for managed acco...
    Ided2ddbf0:Revert "Revert "Revert "Update extras affected by ...
    Ic3ee14242:Revert "Temporarily show a generic title in the ow...
    
    Change-Id: Iec0fcbd24a4e707b6b180de144d45acadf86958b

--- src/com/android/managedprovisioning/provisioning/LandingActivity.java
+++ src/com/android/managedprovisioning/provisioning/LandingActivity.java
@@ -15,6 +15,9 @@
  */
 package com.android.managedprovisioning.provisioning;
 
+import static android.app.admin.DevicePolicyManager.PROVISIONING_TRIGGER_MANAGED_ACCOUNT;
+import static android.app.admin.DevicePolicyManager.PROVISIONING_TRIGGER_PERSISTENT_DEVICE_OWNER;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -59,8 +62,15 @@ public class LandingActivity extends SetupGlifLayoutActivity {
     }
 
     private void initializeUi(ProvisioningParams params) {
-        final int headerResId = R.string.brand_screen_header;
-        final int titleResId = R.string.setup_device_progress;
+        int headerResId = R.string.brand_screen_header;
+        int titleResId = R.string.setup_device_progress;
+
+        // TODO(b/175622930): Remove references to managed account provisioning
+        if (params.provisioningTrigger == PROVISIONING_TRIGGER_MANAGED_ACCOUNT
+                || params.provisioningTrigger == PROVISIONING_TRIGGER_PERSISTENT_DEVICE_OWNER) {
+            headerResId = R.string.account_management_disclaimer_header;
+            titleResId = R.string.account_management_disclaimer_subheader;
+        }
 
         final CustomizationParams customizationParams =
                 CustomizationParams.createInstance(params, this, mUtils);
