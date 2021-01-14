a0fc9d6cd8db098284108071937bdb8e4c75876e
Ram Parameswaran <rampara@google.com>
2021-01-14 15:01:35 -0800

    Scroll to elements on automotive devices before view assertions
    Fixes: 172526111
    Test: atest ManagedProvisioningTests:com.android.managedprovisioning.preprovisioning.terms.TermsActivityTest#expanderTest
    
    Change-Id: I21ea2302b65ea23861fa05224bfa1333a1ef3d31

--- tests/instrumentation/src/com/android/managedprovisioning/preprovisioning/terms/TermsActivityTest.java
+++ tests/instrumentation/src/com/android/managedprovisioning/preprovisioning/terms/TermsActivityTest.java
@@ -21,6 +21,7 @@ import static android.app.admin.DevicePolicyManager.ACTION_PROVISION_MANAGED_PRO
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -38,6 +39,7 @@ import android.provider.Settings;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.filters.FlakyTest;
 import androidx.test.filters.SmallTest;
 import androidx.test.rule.ActivityTestRule;
@@ -53,7 +55,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashMap;
@@ -107,10 +108,15 @@ public class TermsActivityTest {
     @Test
     public void expanderTest() throws InterruptedException {
         PackageManager pm = InstrumentationRegistry.getTargetContext().getPackageManager();
+        // Rely on automotive specific test (expanderTest_automotive) if running on AAOS.
+        if (pm.hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE)) {
+            return;
+        }
+
         boolean isPOSupported = pm.hasSystemFeature(PackageManager.FEATURE_MANAGED_USERS);
         boolean isDOSupported = pm.hasSystemFeature(PackageManager.FEATURE_DEVICE_ADMIN);
         assumeTrue("Device doesn't support managed device or managed profile",
-                 isPOSupported || isDOSupported);
+                isPOSupported || isDOSupported);
         // given an intent with disclaimers
         DisclaimersParam.Disclaimer[] extraDisclaimers = {
                 setUpDisclaimer(HEADER_1, "path1", CONTENT_1),
@@ -120,19 +126,14 @@ public class TermsActivityTest {
         // when an activity is launched
         if (isPOSupported) {
             mActivityRule.launchActivity(
-                        createIntent(ACTION_PROVISION_MANAGED_PROFILE, extraDisclaimers));
+                    createIntent(ACTION_PROVISION_MANAGED_PROFILE, extraDisclaimers));
             // main header is displayed if setting up PO
             onView(withText(HEADER_0_PO)).check(matches(isDisplayed()));
         } else {
             mActivityRule.launchActivity(
-                        createIntent(ACTION_PROVISION_MANAGED_DEVICE, extraDisclaimers));
+                    createIntent(ACTION_PROVISION_MANAGED_DEVICE, extraDisclaimers));
             // main header is displayed if setting up DO
             onView(withText(HEADER_0_DO)).check(matches(isDisplayed()));
-        }
-
-        // TODO (b/172526111) Temporarily skipping the rest as it's broken on automotive.
-        if (pm.hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE)) {
-            return;
         }
 
         // all headers are displayed
@@ -155,6 +156,77 @@ public class TermsActivityTest {
         // TODO: replace with a proper fix; all attempts failed so far
         // Not tried yet: https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/
         Thread.sleep(500);
+
+        // when clicking again on the first one, the last one collapses
+        onView(withText(HEADER_1)).perform(click());
+        onView(withText(CONTENT_3)).check(isNotDisplayed());
+        onView(withText(CONTENT_1)).check(matches(isDisplayed()));
+
+        // check that HTML in disclaimers is respected
+        onView(withText("header2")).perform(click());
+        onView(withId(R.id.disclaimer_content)).check(matches(withText("item1\nitem2\n")));
+    }
+
+    // TODO(b/35613314): remove @FlakyTest once bug fixed
+    @FlakyTest
+    @Test
+    public void expanderTest_automotive() throws InterruptedException {
+        PackageManager pm = InstrumentationRegistry.getTargetContext().getPackageManager();
+        // This test is an automotive specific version of expanderTest. Includes scrolling to ensure
+        // views are on screen.
+        if (!pm.hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE)) {
+            return;
+        }
+
+        boolean isDOSupported = pm.hasSystemFeature(PackageManager.FEATURE_DEVICE_ADMIN);
+        assumeTrue("Device doesn't support managed device", isDOSupported);
+
+        // given an intent with disclaimers
+        DisclaimersParam.Disclaimer[] extraDisclaimers = {
+                setUpDisclaimer(HEADER_1, "path1", CONTENT_1),
+                setUpDisclaimer(HEADER_2, "path2", CONTENT_2_HTML),
+                setUpDisclaimer(HEADER_3_BOTTOM, "path3", CONTENT_3)};
+
+        // when an activity is launched
+        mActivityRule.launchActivity(
+                createIntent(ACTION_PROVISION_MANAGED_DEVICE, extraDisclaimers));
+        // main header is displayed if setting up DO
+        onView(withText(HEADER_0_DO)).check(matches(isDisplayed()));
+
+        // all headers are displayed
+        for (DisclaimersParam.Disclaimer d : extraDisclaimers) {
+            onView(withId(R.id.terms_container)).perform(
+                    RecyclerViewActions.scrollTo(hasDescendant(withText(d.mHeader))));
+            onView(withText(d.mHeader)).check(matches(isDisplayed()));
+        }
+
+        // On automotive screens, elements are larger, scroll back to first header.
+        onView(withId(R.id.terms_container)).perform(
+                RecyclerViewActions.scrollTo(hasDescendant(withText(HEADER_1))));
+        // none of the content is displayed
+        onView(withId(R.id.disclaimer_content)).check(isNotDisplayed());
+
+        // when clicking on one, it expands
+        onView(withText(HEADER_1)).perform(click());
+        onView(withText(CONTENT_1)).check(matches(isDisplayed()));
+
+        // On automotive screens, elements are larger, which may necessitate scrolling to for
+        // header3 to be on screen.
+        onView(withId(R.id.terms_container)).perform(
+                RecyclerViewActions.scrollTo(hasDescendant(withText(HEADER_3_BOTTOM))));
+        // when clicking on another, it collapses the first one, and expands the other one
+        onView(withText(HEADER_3_BOTTOM)).perform(click());
+        onView(withText(CONTENT_1)).check(isNotDisplayed());
+        onView(withText(CONTENT_3)).check(matches(isDisplayed()));
+
+        // TODO: replace with a proper fix; all attempts failed so far
+        // Not tried yet: https://google.github
+        // .io/android-testing-support-library/docs/espresso/idling-resource/
+        Thread.sleep(500);
+
+        // On automotive screens, elements are larger, scroll back to first header.
+        onView(withId(R.id.terms_container)).perform(
+                RecyclerViewActions.scrollTo(hasDescendant(withText(HEADER_1))));
 
         // when clicking again on the first one, the last one collapses
         onView(withText(HEADER_1)).perform(click());
